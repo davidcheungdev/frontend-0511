59f048b7bc45cd13ef76832a6942e5004f08814c
David Cheung <davidcheung@live.ca>
2021-05-14 12:59:04 -0400

    fixup! fixup! Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function SpinningThing () {
   return <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="foo"><
+          img src={logo} className="App-logo" alt="logo" />
+        </Link>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
   </div>
 }
@@ -26,7 +20,14 @@ function App() {
     <Router>
       <Switch>
         <Route path="/foo">
-          <p>foo bar</p>
+          <div className="App">
+            <header className="App-header">
+            <p>foo bar</p>
+            <Link to="/">
+              to home
+            </Link>
+            </header>
+          </div>
         </Route>
         <Route path="/*">
           <SpinningThing />
